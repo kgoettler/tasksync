3b7fc7f57750a6ec8fd337e94889706d35ad8071
Ken Goettler <goettlek@gmail.com>
2023-08-30 06:57:51 -0400

    chore: fix linter complaints

--- test/test_taskwarrior.py
+++ test/test_taskwarrior.py
@@ -2,8 +2,13 @@
 
 import pytest
 
-from todoist_api_python.models import Task as TodoistTask, Due as TodoistDue
-from tasksync.models import TaskwarriorTask, TaskwarriorDatetime, TaskwarriorPriority, TaskwarriorStatus
+#from todoist_api_python.models import Task as TodoistTask, Due as TodoistDue
+from tasksync.models import (
+    TaskwarriorTask,
+    TaskwarriorDatetime,
+    TaskwarriorPriority,
+    TaskwarriorStatus,
+)
 
 from test_data import get_task, get_todoist, get_taskwarrior_input
 
@@ -11,7 +16,7 @@ class TestTaskwarrior:
 
     def test_from_taskwarrior_str(self):
         json_data = get_taskwarrior_input(return_type='str')
-        task = TaskwarriorTask.from_taskwarrior(json_data=json_data)
+        _ = TaskwarriorTask.from_taskwarrior(json_data=json_data)
         assert True
 
     @pytest.mark.parametrize('due_datetime', [True, False])
@@ -23,7 +28,7 @@ class TestTaskwarrior:
             assert task.due.strftime('%H%M%S') == '040000'
         else:
             assert task.due.strftime('%H%M%S') == '130000' 
-        assert task.due.tzinfo.key == 'UTC'
+        assert task.due.tzinfo.key == 'UTC' # type: ignore
         assert isinstance(task.priority, TaskwarriorPriority)
         assert task.priority == TaskwarriorPriority.M
         assert task.priority.value == 2
@@ -57,8 +62,8 @@ class TestTaskwarrior:
         todoist = get_todoist(due_datetime=due_datetime)
         task_from_todoist = TaskwarriorTask.from_todoist(todoist)
         assert isinstance(task_from_todoist.due, TaskwarriorDatetime)
-        assert task_from_todoist.due.strftime('%Y%m%dT%H%M%SZ') == task.due.strftime('%Y%m%dT%H%M%SZ')
-        assert task_from_todoist.due.tzinfo.key == 'UTC'
+        assert task_from_todoist.due.strftime('%Y%m%dT%H%M%SZ') == task.due.strftime('%Y%m%dT%H%M%SZ') # type: ignore
+        assert task_from_todoist.due.tzinfo.key == 'UTC' # type: ignore
         assert isinstance(task_from_todoist.priority, TaskwarriorPriority)
         assert task_from_todoist.priority == TaskwarriorPriority.M
         assert task_from_todoist.priority.value == 2
