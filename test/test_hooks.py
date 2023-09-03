777babb3b6dac6037892b7663d05402f092a7be8
Ken Goettler <goettlek@gmail.com>
2023-09-03 09:05:02 -0400

    test(hooks): disable hook tests
    until I can establish a way to test sockets

--- test/test_hooks.py
+++ test/test_hooks.py
@@ -1,29 +1,13 @@
 import pytest
 
 from os.path import dirname, join
-from types import SimpleNamespace
 import json
 
 from tasksync.hooks import on_add, on_modify
 from tasksync.server import TasksyncClient
 from tasksync.todoist import TodoistSyncDataStore
 
-class DummyAPI:
-    def __init__(self):
-        return
-    
-    def add_task(self, **kwargs):
-        return SimpleNamespace(
-            id=123,
-        )
-    
-    def delete_task(self, **kwargs):
-        return SimpleNamespace()
-    
-    def update_task(self, **kwargs):
-        return SimpleNamespace(
-            id=123,
-        )
+from test_data import get_task
 
 class DummySyncAPI:
     def __init__(self):
@@ -48,7 +32,6 @@ class DummySync:
         self.api = DummySyncAPI()
         self.store = TodoistSyncDataStore(basedir=join(dirname(__file__), 'data'))
 
-from test_data import get_task
 
 @pytest.fixture
 def task():
@@ -70,70 +53,73 @@ def sync():
 def client():
     return TasksyncClient()
 
-def test_on_add(sync, client):
-    task_json, feedback = on_add(
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","project":"Inbox","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
-        sync,
-        client,
-    )
-    task_json = json.loads(task_json)
-    #assert task_json['todoist'] == 123
-    #assert task_json['timezone'] == 'America/New_York'
-    assert feedback == 'Todoist: item created'
+@pytest.mark.skip(reason="Need to determine how to test sockets")
+class TestHooks:
 
-def test_on_add_new_project(sync, client):
-    task_json, feedback = on_add(
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","project":"Work","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
-        sync,
-        client,
-    )
-    task_json = json.loads(task_json)
-    #assert task_json['todoist'] == 123
-    #assert task_json['timezone'] == 'America/New_York'
-    assert task_json['project'] == 'Work'
-    assert feedback == 'Todoist: item created'
+    def test_on_add(self, sync, client):
+        task_json, feedback = on_add(
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","project":"Inbox","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
+            sync,
+            client,
+        )
+        task_json = json.loads(task_json)
+        #assert task_json['todoist'] == 123
+        #assert task_json['timezone'] == 'America/New_York'
+        assert feedback == 'Todoist: item created'
 
-def test_on_modify_update(sync, client):
-    task_json, feedback = on_modify(
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
-        '{"id":3,"description":"Test Update","entry":"20230827T232837Z","modified":"20230827T233228Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
-        sync,
-        client,
-    )
-    task_json = json.loads(task_json)
-    assert task_json['todoist'] == 123
-    assert task_json['description'] == 'Test Update'
-    assert feedback == 'Todoist: item updated'
+    def test_on_add_new_project(self, sync, client):
+        task_json, feedback = on_add(
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","project":"Work","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
+            sync,
+            client,
+        )
+        task_json = json.loads(task_json)
+        #assert task_json['todoist'] == 123
+        #assert task_json['timezone'] == 'America/New_York'
+        assert task_json['project'] == 'Work'
+        assert feedback == 'Todoist: item created'
 
-def test_on_modify_delete(sync, client):
-    task_json, feedback = on_modify(
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T233228Z","status":"deleted","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
-        sync,
-        client,
-    )
-    task_json = json.loads(task_json)
-    assert feedback == 'Todoist: item deleted'
+    def test_on_modify_update(self, sync, client):
+        task_json, feedback = on_modify(
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
+            '{"id":3,"description":"Test Update","entry":"20230827T232837Z","modified":"20230827T233228Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
+            sync,
+            client,
+        )
+        task_json = json.loads(task_json)
+        assert task_json['todoist'] == 123
+        assert task_json['description'] == 'Test Update'
+        assert feedback == 'Todoist: item updated'
 
-def test_on_modify_missing(sync, client):
-    task_json, feedback = on_modify(
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
-        '{"id":3,"description":"Test Update","entry":"20230827T232837Z","modified":"20230827T233228Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
-        sync,
-        client
-    )
-    task_json = json.loads(task_json)
-    assert task_json['description'] == 'Test Update'
-    #assert task_json['todoist'] == 123
-    #assert task_json['timezone'] == 'America/New_York'
-    assert feedback == 'Todoist: item added (did not exist)' 
+    def test_on_modify_delete(self, sync, client):
+        task_json, feedback = on_modify(
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T233228Z","status":"deleted","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
+            sync,
+            client,
+        )
+        task_json = json.loads(task_json)
+        assert feedback == 'Todoist: item deleted'
 
-def test_on_modify_noop(sync, client):
-    task_json, feedback = on_modify(
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
-        '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230828T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
-        sync,
-        client
-    )
-    task_json = json.loads(task_json)
-    assert feedback == 'Todoist: update not required'
+    def test_on_modify_missing(self, sync, client):
+        task_json, feedback = on_modify(
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
+            '{"id":3,"description":"Test Update","entry":"20230827T232837Z","modified":"20230827T233228Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0}',
+            sync,
+            client
+        )
+        task_json = json.loads(task_json)
+        assert task_json['description'] == 'Test Update'
+        #assert task_json['todoist'] == 123
+        #assert task_json['timezone'] == 'America/New_York'
+        assert feedback == 'Todoist: item added (did not exist)' 
+
+    def test_on_modify_noop(self, sync, client):
+        task_json, feedback = on_modify(
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230827T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
+            '{"id":3,"description":"Test 1","entry":"20230827T232837Z","modified":"20230828T232837Z","status":"pending","uuid":"5da82ec9-e85b-47ac-b0c6-9e3486f9fb74","urgency":0,"todoist":123}',
+            sync,
+            client
+        )
+        task_json = json.loads(task_json)
+        assert feedback == 'Todoist: update not required'
